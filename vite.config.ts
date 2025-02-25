7a993e881d4970aee0d01173058e488e16aeecf4
tomicaucino <tcaucino@hotmail.com>
2025-02-25 14:48:51 -0300

    upload page

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/invitart',
+  base: '/invitart2',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src'),
